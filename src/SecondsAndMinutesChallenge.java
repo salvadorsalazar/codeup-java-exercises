f3360486084f108bb7e9fa2f7cb83c41bd6b2d4e
salvador salazar <salvador.g.salazar79@gmail.com>
2021-09-12 20:51:32 -0500

    added src/SecondsAndMinutesChallenge.java practice work

---
+++ src/SecondsAndMinutesChallenge.java
@@ -0,0 +1,57 @@
+import com.sun.jdi.VMMismatchException;
+
+public class SecondsAndMinutesChallenge {
+
+
+    public static void main(String[] args) {
+        System.out.println(getDurationString(65,45));
+        System.out.println(getDurationString(3945));
+
+    }
+
+    private static String getDurationString(long minutes,long seconds){
+        if((minutes < 0) || (seconds <0)||(seconds > 59) ){
+            return "Invalid Value";
+        }
+        long hours = minutes / 60;
+        long remainingMinuttes = minutes %60;
+
+        String hoursString = hours + "h";
+        if(hours <10){
+            hoursString ="0" + hoursString;
+        }
+
+        String minutesString = remainingMinuttes + "m";
+        if(remainingMinuttes <10) {
+            minutesString = "0" + minutesString;
+        }
+
+        String secondsString = seconds + "s";
+        if(seconds <10) {
+            secondsString = "0" + secondsString;
+        }
+
+        return hoursString + " " + minutesString + " " + secondsString+ " ";
+    }
+
+    private static String getDurationString(long seconds){
+
+        if(seconds < 0){
+            return "invalid";
+        }
+
+        long minutes =  seconds /60;
+        long remainingSeconds = seconds %60;
+
+//acceptble as well , but below it is put into one line
+//        String x  = getDurationString(minutes,seconds);
+//        return x;
+        return getDurationString(minutes,remainingSeconds);
+
+
+
+    }
+
+
+
+}
