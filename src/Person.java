2bf8fe96178fe6db4a91b64f35eabe7d38228df0
salvador salazar <salvador.g.salazar79@gmail.com>
2021-09-14 10:34:31 -0500

    correcting files

--- src/Person.java
+++ src/Person.java
@@ -5,16 +5,13 @@ public class Person {
     public String name;
 
     public String getName(){
-//TODO: return the person's name
         return this.name;
     }
 
     public void setName(String name){
-//TODO: change the name property to the passed value
         this.name = name;
     }
     public void sayHello(){
-//TODO: print a message to the console using the person's name
         System.out.println("Hello, " + this.name + ".");
     }
 
@@ -23,10 +20,7 @@ public class Person {
     }
 
     public static void main(String[] args) {
-//        Person person1 = new Person("jeff");
-//        System.out.println(person1.name);
-//        person1.setName("notJeff");
-//        System.out.println(person1.name);
+
     }
 }
 
