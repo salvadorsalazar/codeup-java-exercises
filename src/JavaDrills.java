16133c0e3cf36ed31e9253726e64f34436de1568
salvador salazar <salvador.g.salazar79@gmail.com>
2021-10-14 10:04:12 -0500

    flipOuterCase complete java drill warmup .

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -1,32 +1,37 @@
 public class JavaDrills {
 
-//    WORK IN PROGRESS
+//
+//    COMPLETE
     public static void main(String[] args) {
-        flipOuterCase("cat");
-        flipOuterCase("CaT");
-        flipOuterCase("cAt");
+        flipOuterCase("cat");// CaT
+        flipOuterCase("CaT");// cat
+        flipOuterCase("caT");// Cat
+        flipOuterCase("cAt");// CAT
 
-
+//////
+//        System.out.println(flipOuterCase(“cat”));
+//        System.out.println(flipOuterCase(“CaT”));
+//        System.out.println(flipOuterCase(“caT”));
+//        System.out.println(flipOuterCase(“cAt”));
     }
-    public static  String flipOuterCase(String str){
+    public static void flipOuterCase(String str){
 
         char[] ch= str.toCharArray();
         for (int i =0;i<ch.length;i++){
             if(Character.isLowerCase(ch[0])){
                 ch[0] = Character.toUpperCase(ch[0]);
+
             }else if(Character.isUpperCase(ch[0])){
                 ch[0]= Character.toLowerCase(ch[0]);
             }
             if(Character.isLowerCase(ch[2])){
-                ch[0] = Character.toUpperCase(ch[2]);
+                ch[2] = Character.toUpperCase(ch[2]);
             }else if(Character.isUpperCase(ch[2])){
-                ch[0]= Character.toLowerCase(ch[2]);
+                ch[2]= Character.toLowerCase(ch[2]);
             }
-            String changedCase= new String(ch);
-            System.out.println(changedCase);
 
 
-
-        }return null;
+        }  String changedCase= new String(ch);
+        System.out.println(changedCase);
     }
 }
